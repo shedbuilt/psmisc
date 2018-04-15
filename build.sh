2125f25ee7755558c32e1dfaad269d4372ddb83e
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:27:34 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./configure --prefix=/usr
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-mkdir -v ${SHED_FAKEROOT}/bin
-mv -v ${SHED_FAKEROOT}/usr/bin/fuser   ${SHED_FAKEROOT}/bin
-mv -v ${SHED_FAKEROOT}/usr/bin/killall ${SHED_FAKEROOT}/bin
+make -j $SHED_NUM_JOBS
+make DESTDIR=${SHED_FAKE_ROOT} install
+mkdir -v ${SHED_FAKE_ROOT}/bin
+mv -v ${SHED_FAKE_ROOT}/usr/bin/fuser   ${SHED_FAKE_ROOT}/bin
+mv -v ${SHED_FAKE_ROOT}/usr/bin/killall ${SHED_FAKE_ROOT}/bin

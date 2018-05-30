1ee66c160da043b9f19b582fc3b15bb2324fc77c
Auston Stewart <auston@shedbuilt.net>
2018-05-30 06:52:34 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-./configure --prefix=/usr
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
-mkdir -v ${SHED_FAKE_ROOT}/bin
-mv -v ${SHED_FAKE_ROOT}/usr/bin/fuser   ${SHED_FAKE_ROOT}/bin
-mv -v ${SHED_FAKE_ROOT}/usr/bin/killall ${SHED_FAKE_ROOT}/bin
+# Configure
+./configure --prefix=/usr &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Rearrange
+mkdir -v "${SHED_FAKE_ROOT}/bin" &&
+mv -v "${SHED_FAKE_ROOT}/usr/bin/fuser"   "${SHED_FAKE_ROOT}/bin" &&
+mv -v "${SHED_FAKE_ROOT}/usr/bin/killall" "${SHED_FAKE_ROOT}/bin"
